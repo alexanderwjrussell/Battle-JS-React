76a158e9fb407d1a6e2feffc53c22ff635f831ed
Alexander Russell <awjrussell@live.co.uk>
2017-07-27 09:15:54 +0100

    Initial set up

---
+++ spec/battleAppSpec.js
@@ -0,0 +1,8 @@
+describe("BattleApp.js", function() {
+
+  describe("#", function() {
+    it("should be ", function() {
+      expect(...).toEqual(...);
+    });
+  });
+});
---
+++ spec/playerSpec.js
@@ -0,0 +1,8 @@
+describe("player.js", function() {
+
+  describe("#name", function() {
+    it("should be able to get player 1 name", function() {
+      expect(player1.name).toEqual("Ali");
+    });
+  });
+});
